8ed74c5cde17009337c21aa0cbd3ee1cd062e355
kazunari-kamata <14287197+kazunari-kamata@users.noreply.github.com>
2026-05-28 19:16:49 +0900

    Add samples for simple switch blocks

--- tests/sample_paths_test.go
+++ tests/sample_paths_test.go
@@ -15,6 +15,7 @@ func TestSampleFilesExist(t *testing.T) {
 		"../samples/basic/control_flow/main.go",
 		"../samples/basic/early_return/main.go",
 		"../samples/basic/loops/main.go",
+		"../samples/basic/switches/main.go",
 		"../samples/unsupported/control_flow/main.go",
 		"../samples/unsupported/functions/main.go",
 	}
